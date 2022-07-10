e2bf1854d81d3921dadd085cbc14c797e92c51e5
Hyoung Jin <drj9802@gmail.com>
2022-07-11 02:38:33 +0900

    Login Intent 추가

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlin_practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,10 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
     }
     fun Login(v:View){
         println("All RIGHT")
+
+        var intent = Intent(this, LoginSuccessActivity::class.java)
+        intent.putExtra("이름","")
+        startActivity(intent)
+
     }
 }
